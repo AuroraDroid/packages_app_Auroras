e529de8afa0c630bbc45fa6a5d41fda991f18e93
neobuddy89 <neobuddy89@gmail.com>
2024-07-17 13:21:37 +0530

    Auroras: Port brightness slider changes [2/2]
    Change-Id: I3e895f214a04b8509397eadbde1d1043c481788a
    Signed-off-by: afterallafk <shivamatiet2001@gmail.com>

--- src/com/aurora/droid/fragments/NotificationSettings.java
+++ src/com/aurora/droid/fragments/NotificationSettings.java
@@ -6,17 +6,35 @@ import android.os.Bundle;
 import com.android.settings.R;
 
 import androidx.preference.Preference;
+import androidx.preference.PreferenceGroup;
 import androidx.preference.PreferenceCategory;
 import androidx.preference.PreferenceScreen;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+import androidx.preference.PreferenceFragment;
+import androidx.preference.SwitchPreference;
+import android.provider.Settings;
 
 import com.android.settings.SettingsPreferenceFragment;
 
 public class NotificationSettings extends SettingsPreferenceFragment {
 
+    private static final String KEY_QS_SHOW_AUTO_BRIGHTNESS = "qs_show_auto_brightness";
+
     @Override
-    public void onCreate(Bundle icicle) {
-        super.onCreate(icicle);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.aurora_notifications);
+
+	PreferenceScreen preferenceScreen = getPreferenceScreen();
+        Preference qsShowAutoBrightnessPreference = preferenceScreen.findPreference(KEY_QS_SHOW_AUTO_BRIGHTNESS);
+
+        if (qsShowAutoBrightnessPreference != null) {
+            boolean automaticBrightnessAvailable = getContext().getResources().getBoolean(
+                    com.android.internal.R.bool.config_automatic_brightness_available);
+            if (!automaticBrightnessAvailable) {
+                qsShowAutoBrightnessPreference.setVisible(false);
+            }
+        }
     }
 
     @Override
